a08d2a64841cebbdcaeef71fadc8a19bb378dcb5
Juma1999 <jumakraimesh@gmail.com>
2023-08-11 16:21:16 -0700

    another change

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: Stuff
  * Return: 0 (success)
 */
+
 int main(void)
 {
 	char ch = 'a';
+	char CH = 'A';
 
-	while (ch <= 'Z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
 	putchar('\n');
+
 	return (0);
 }

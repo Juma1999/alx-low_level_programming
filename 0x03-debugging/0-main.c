ad1da7fadbac8a7ca2af92e4ba514247333d4162
Juma1999 <jumakraimesh@gmail.com>
2023-08-17 16:26:50 -0700

    another change

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -13,3 +13,13 @@ int main(void)
 	positive_or_negative(i);
 	return (0);
 }
+void positive_or_negative(int n)
+{
+	/* your code goes there */
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+}

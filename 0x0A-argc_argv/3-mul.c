09f2a3c981d7b3aa34b0efad3fbd670cac493d34
Juma1999 <jumakraimesh@gmail.com>
2023-09-01 17:22:54 -0700

    another change

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,11 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 	printf("%d\n", atoi(argv[1] * argv[2]));
+	return (0);
 	}
 	else
 	{
 	printf("Error");
+	return (1);
 	}
-	return (0);
 }
